81b73c2d9555da5b37537ea763505fb2990d4d58
Harshitwat <67504108+Harshitwat@users.noreply.github.com>
2020-07-28 15:20:34 +0530

    2nd project

---
+++ jk2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my second project");
+            Console.ReadKey();
+        }
+    }
+}
